51d9c2c2da64a67011c41f84f3e4c00af5485216
danielmoessner <github@moesibert.de>
2021-12-10 12:06:11 +0100

    beispiel 2

--- apps/books/views.py
+++ apps/books/views.py
@@ -1,20 +1,26 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import render
-
+import requests
 
 class BooksView(APIView):
     def get(self, *args, **kwargs):
+        sales = requests.get('http://localhost:8230/')
+        sales = sales.json()
         books = [
             {
+                'id': 1,
                 "name": "1984",
                 "author": "George Orwell",
                 "published": 1949
             },
             {
+                'id': 2,
                 "name": "The Gulag Archipelago",
                 "author": "Aleksandr Solzhenitsyn",
                 "published": 2018
             }
         ]
+        for book in books:
+            book['sales'] = len(list(filter(lambda item: item['book'] == book['id'], sales)))
         return Response(books)
